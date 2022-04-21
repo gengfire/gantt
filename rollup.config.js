e68cb018f8efe1c23d667965180e783cb48e3e51
廖庚生 <liaogengsheng@boswinner.com>
2022-04-21 11:20:13 +0800

    umm

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,19 @@
 import sass from 'rollup-plugin-sass';
 import uglify from 'rollup-plugin-uglify';
-import merge from 'deepmerge';
 
-const dev = {
+const prod = {
     input: 'src/index.js',
     output: {
         name: 'Gantt',
-        file: 'dist/frappe-gantt-bigdata.js',
-        format: 'iife'
+        file: 'dist/index.js',
+        format: 'umd'
     },
     plugins: [
         sass({
             output: 'dist/gantt.css'
-        })
+        }),
+        uglify()
     ]
 };
-const prod = merge(dev, {
-    output: {
-        file: 'dist/frappe-gantt-bigdata.min.js'
-    },
-    plugins: [uglify()]
-});
 
-export default [dev, prod];
+export default [prod];
